0c4091e2cb92eddc71474dd3bfa26b8cc284e7d9
USER <USER@DESKTOP-2GO45DL>
2022-09-17 13:53:54 +0530

    Add project files.

---
+++ DapperCrud/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public EmployeeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Employee>>>GetAllEmployees()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<Employee> Emp = await SelectAllEmployes(connection);
+            return Ok(Emp);
+        }
+
+        [HttpGet("{EmpId}")]
+
+        public async Task<ActionResult<Employee>> GetEmp(int EmpId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var Empl = await connection.QueryFirstAsync<Employee>("select * from Employees where empid = @EmpID",
+                new {EmpID = EmpId});
+            return Ok(Empl);
+        }
+        [HttpPost]
+
+        public async Task<ActionResult<List<Employee>>> CreateEmployee(Employee emp)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into employees (empname, email, age) values (@EmpName, @Email, @Age)", emp);
+            return Ok(await SelectAllEmployes(connection));
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult<List<Employee>>> UpdateEmployee(Employee emp)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update employees set empname = @EmpName, email = @Email, age = @Age where empid = @EmpID", emp);
+            return Ok(await SelectAllEmployes(connection));
+        }
+
+        [HttpDelete("{EmpId}")]
+
+        public async Task<ActionResult<List<Employee>>> eleteEmployee(int EmpId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from employees where empid = @EmpID", new { EmpID = EmpId});
+            return Ok(await SelectAllEmployes(connection));
+        }
+
+
+
+        private static async Task<IEnumerable<Employee>> SelectAllEmployes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Employee>("select * from Employees");
+        }
+
+    }
+}
---
+++ DapperCrud/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace DapperCrud
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+    }
+}
